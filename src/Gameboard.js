9c948dadfdcb5400559807f6b52da872c30f3605
aadeleye <adeleyeadeyemib@gmail.com>
2022-02-04 13:45:16 +0100

    new dice done

--- src/Gameboard.js
+++ src/Gameboard.js
@@ -1,106 +1,6 @@
 import React from "react";
 import { nanoid } from "nanoid";
-
-function Die1(props) {
-  return (
-    <div
-      className="die1"
-      style={{ backgroundColor: `${props.isHeld ? "#E4B16E" : "#ffffff"}` }}
-      onClick={props.clickHandler}
-    >
-      <span className="dot"></span>
-    </div>
-  );
-}
-
-function Die2(props) {
-  return (
-    <div
-      className="die2"
-      style={{ backgroundColor: `${props.isHeld ? "#E4B16E" : "#ffffff"}` }}
-      onClick={props.clickHandler}
-    >
-      <span className="dot"></span>
-      <span
-        className="dot"
-        style={{
-          justifySelf: "end",
-        }}
-      ></span>
-    </div>
-  );
-}
-
-function Die3(props) {
-  return (
-    <div
-      className="die3"
-      style={{ backgroundColor: `${props.isHeld ? "#E4B16E" : "#ffffff"}` }}
-      onClick={props.clickHandler}
-    >
-      <span className="dot"></span>
-      <span
-        className="dot"
-        style={{
-          justifySelf: "center",
-        }}
-      ></span>
-      <span
-        className="dot"
-        style={{
-          justifySelf: "right",
-        }}
-      ></span>
-    </div>
-  );
-}
-
-function Die4(props) {
-  return (
-    <div
-      className="die4"
-      style={{ backgroundColor: `${props.isHeld ? "#E4B16E" : "#ffffff"}` }}
-      onClick={props.clickHandler}
-    >
-      <span className="dot"></span>
-      <span className="dot"></span>
-      <span className="dot"></span>
-      <span className="dot"></span>
-    </div>
-  );
-}
-
-function Die5(props) {
-  return (
-    <div
-      className="die5"
-      style={{ backgroundColor: `${props.isHeld ? "#E4B16E" : "#ffffff"}` }}
-      onClick={props.clickHandler}
-    >
-      <span className="dot"></span>
-      <span className="dot"></span>
-      <span className="dot"></span>
-      <span className="dot"></span>
-    </div>
-  );
-}
-
-function Die6(props) {
-  return (
-    <div
-      className="die6"
-      style={{ backgroundColor: `${props.isHeld ? "#E4B16E" : "#ffffff"}` }}
-      onClick={props.clickHandler}
-    >
-      <span className="dot"></span>
-      <span className="dot"></span>
-      <span className="dot"></span>
-      <span className="dot"></span>
-      <span className="dot"></span>
-      <span className="dot"></span>
-    </div>
-  );
-}
+import {Die1, Die2, Die3, Die4, Die5, Die6} from "./Dice"
 
 function Instruction() {
   return (
@@ -133,6 +33,12 @@ function Gameboard() {
   }
 
   const [dice, setDice] = React.useState(() => allNewDice());
+
+  const [tenzies, setTenzies] = React.useState(false);
+
+  React.useEffect(() => {
+    console.log(dice.every(die => die.value === dice[0].value))
+  })
 
   function holdDice(id) {
     setDice(
